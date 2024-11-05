55a35e397f7c0006cadc92855aa1d9203c0cfd44
tinhpt <tinhphan31199@gmail.com>
2024-11-05 22:19:46 +0700

    first commit

---
+++ src/main/java/com/example/restservice/Greeting.java
@@ -0,0 +1,11 @@
+package com.example.restservice;
+
+public class Greeting {
+    public Greeting(long Id, String Content) {
+        this.Id = Id;
+        this.Content = Content;
+    }
+
+    public long Id;
+    public String Content;
+}
